d83c5c11eb2e02d4b0d1d687020024348ad4ed78
Joel Gottsegen <djoeligy@gmail.com>
2019-02-03 17:06:04 -0600

    Starting dialogue system.

---
+++ FantasyGame/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- FantasyGame/Assets/Scripts/TargetHandler.cs
+++ FantasyGame/Assets/Scripts/TargetHandler.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine.SceneManagement;
+using System;
 
 public class TargetHandler : MonoBehaviour
 {
@@ -13,12 +15,13 @@ public class TargetHandler : MonoBehaviour
     public Texture2D door; //Cursor for doorways
     public Texture2D combat; //Cursor for combat actions
 
-    
+    enum CursorType { Pointer, Door, Target, Combat };
+    CursorType cursorType;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cursorType = CursorType.Pointer;
     }
 
     // Update is called once per frame
@@ -28,21 +31,36 @@ public class TargetHandler : MonoBehaviour
         Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
-            Transform objectHit = hit.transform;
-            print(objectHit);
+            UpdateTargetCursorIcon(hit);
+            HandleUserInput();
         }
+    }
+
+    private void UpdateTargetCursorIcon(RaycastHit hit)
+    {
         if (hit.collider.gameObject.tag == "Door" && hit.distance < 3)
         {
+            cursorType = CursorType.Door;
             Target targetCursor = FindObjectOfType<Target>();
             targetCursor.GetComponent<RawImage>().texture = door;
             targetCursor.GetComponent<RectTransform>().sizeDelta = new Vector2(20, 20);
-            SceneManager.LoadScene("Shuli's House");
         }
         else
         {
+            cursorType = CursorType.Pointer;
             Target targetCursor = FindObjectOfType<Target>();
             targetCursor.GetComponent<RectTransform>().sizeDelta = new Vector2(5, 5);
             targetCursor.GetComponent<RawImage>().texture = pointer;
+        }
+    }
+
+    private void HandleUserInput()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Interact")) {
+            if(cursorType == CursorType.Door)
+            {
+                SceneManager.LoadScene("Shuli's House");
+            }
         }
     }
 }
