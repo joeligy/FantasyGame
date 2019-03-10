2be756f786e08343a45c10279e81c2a4b665c355
Joel Gottsegen <djoeligy@gmail.com>
2019-03-09 22:13:16 -0800

    Working on combat!!

---
+++ FantasyGame/Assets/Enemies/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Enemy : MonoBehaviour
+{
+
+    AICharacterControl aiCharacterControl;
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aiCharacterControl.target = player.transform;
+    }
+}
