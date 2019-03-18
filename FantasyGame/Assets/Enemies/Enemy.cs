11ceb0f1b1fec15937880c9ee1d3735a78c88fed
Joel Gottsegen <djoeligy@gmail.com>
2019-03-17 17:41:33 -0700

    Working on explosions in combat.

--- FantasyGame/Assets/Enemies/Enemy.cs
+++ FantasyGame/Assets/Enemies/Enemy.cs
@@ -5,21 +5,43 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public bool isAlive = true;
+
     [SerializeField] float health = 100f;
 
     NavMeshAgent nav;
     Player player;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<Player>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isAlive) return;
+
         nav.destination = player.transform.position;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        isAlive = false;
+        nav.isStopped = true;
+        animator.SetTrigger("Death");
     }
 }
---
+++ FantasyGame/Assets/Player & Navigation/Spells/FireBall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+
+    [SerializeField] float damageCaused = 10f;
+    [SerializeField] float maxDistance = 100f;
+    [SerializeField] GameObject fireBallExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player>()) return;
+
+        Enemy enemy = other.gameObject.GetComponent<Enemy>();
+        if (enemy && enemy.isAlive)
+        {
+            enemy.TakeDamage(damageCaused);
+        }
+        Explode();
+    }
+
+    void Explode()
+    {
+        Instantiate(fireBallExplosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
